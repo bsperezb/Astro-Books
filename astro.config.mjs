d9c30b02917475c7c3b94e7503185b4662e02716
Brayan Perez <brayanperezbuitrago@gmail.com>
2024-10-19 02:15:55 -0500

    Added relative path to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://bsperezb.github.io',
   base: '/Astro-Books/',
   output: 'static'
 });
